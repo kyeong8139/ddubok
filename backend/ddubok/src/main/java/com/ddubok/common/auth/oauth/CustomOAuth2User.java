425fdcc0c341e19d85cee6a3df76d31894cffd0d
최다환 <sinphoto@naver.com>
2024-10-21 16:38:10 +0900

    [Feat] 소셜 로그인 스켈레톤 코드 작성

---
+++ backend/ddubok/src/main/java/com/ddubok/common/auth/oauth/CustomOAuth2User.java
@@ -0,0 +1,25 @@
+package com.ddubok.common.auth.oauth;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
---
+++ backend/ddubok/src/main/java/com/ddubok/common/auth/registration/SocialClientRegistration.java
@@ -0,0 +1,28 @@
+package com.ddubok.common.auth.registration;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocialClientRegistration {
+
+    public ClientRegistration kakaoClientRegistration() {
+
+        return null;
+    }
+
+    public ClientRegistration naverClientRegistration() {
+
+        return null;
+    }
+
+    public ClientRegistration metaClientRegistration() {
+
+        return null;
+    }
+
+    public ClientRegistration googleClientRegistration() {
+
+        return null;
+    }
+}
