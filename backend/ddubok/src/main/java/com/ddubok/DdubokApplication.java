048b000140557fd69429e68b265411b31b151793
박성혁 <goldenkiwi971202@gmail.com>
2024-10-21 10:45:48 +0900

    [init] 폴더구조 생성

---
+++ backend/ddubok/src/main/java/com/ddubok/DdubokApplication.java
@@ -0,0 +1,13 @@
+package com.ddubok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdubokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdubokApplication.class, args);
+    }
+
+}
---
+++ backend/ddubok/src/test/java/com/ddubok/DdubokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddubok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdubokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
