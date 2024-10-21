bae526989ae3c5953873a5d2e4abfe23eac8ff7c
김경민 <yogurt2819@gmail.com>
2024-10-21 15:48:03 +0900

    [feat] Card dto 스켈레톤 코드 추가

---
+++ backend/ddubok/src/main/java/com/ddubok/api/card/dto/request/DeleteCardReq.java
@@ -0,0 +1,10 @@
+package com.ddubok.api.card.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class DeleteCardReq {
+
+}
---
+++ backend/ddubok/src/main/java/com/ddubok/api/card/dto/response/GetCardDetailRes.java
@@ -0,0 +1,10 @@
+package com.ddubok.api.card.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class GetCardDetailRes {
+
+}
