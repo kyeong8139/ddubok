8de8584ab458783394727a9f944b2f30e3f623c2
김경민 <yogurt2819@gmail.com>
2024-10-21 17:36:12 +0900

    [feat] ResponseCode 추가

--- backend/ddubok/src/main/java/com/ddubok/common/template/response/ResponseCode.java
+++ backend/ddubok/src/main/java/com/ddubok/common/template/response/ResponseCode.java
@@ -10,7 +10,10 @@ public enum ResponseCode {
     // 200 - 성공
     OK("200", "성공"),
     CREATED("201", "생성됨"),
-    DELETED("202", "삭제됨");
+    DELETED("202", "삭제됨"),
+
+    // 300 - card
+    FAIL_TO_UPLOAD_FILE("300", "카드 이미지 업로드 실패");
 
     private String code;
     private String message;
