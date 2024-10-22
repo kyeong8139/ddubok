ca1c8026ec1ec046f07050bc3cae500e6edbfac8
김경민 <yogurt2819@gmail.com>
2024-10-22 11:15:05 +0900

    [feat] error 코드 추가

--- backend/ddubok/src/main/java/com/ddubok/common/template/response/ResponseCode.java
+++ backend/ddubok/src/main/java/com/ddubok/common/template/response/ResponseCode.java
@@ -12,8 +12,14 @@ public enum ResponseCode {
     CREATED("201", "생성됨"),
     DELETED("202", "삭제됨"),
 
-    // 300 - card
-    FAIL_TO_UPLOAD_FILE("300", "카드 이미지 업로드 실패");
+    // 300 - S3
+    FAIL_TO_UPLOAD_FILE("300", "카드 이미지 업로드 실패"),
+    
+    // 400 - member
+    MEMBER_NOT_FOUND("400", "멤버를 찾을 수 없음"),
+
+    // 500 - season
+    SEASON_NOT_FOUND("500", "시즌을 찾을 수 없음");
 
     private String code;
     private String message;
