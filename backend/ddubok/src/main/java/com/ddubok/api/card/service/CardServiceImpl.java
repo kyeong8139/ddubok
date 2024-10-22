12a3c04656102e7696a22500d884a949b777b1cf
김경민 <yogurt2819@gmail.com>
2024-10-22 11:13:40 +0900

    [feat] 카드 등록 서비스 구현

--- backend/ddubok/src/main/java/com/ddubok/api/card/service/CardServiceImpl.java
+++ backend/ddubok/src/main/java/com/ddubok/api/card/service/CardServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ddubok.api.card.service;
 
+import com.ddubok.api.admin.exception.SeasonNotFoundException;
+import com.ddubok.api.admin.repository.SeasonRepository;
 import com.ddubok.api.card.dto.request.CreateCardReqDto;
 import com.ddubok.api.card.dto.request.DeleteCardReq;
+import com.ddubok.api.card.entity.Card;
 import com.ddubok.api.card.repository.CardRepository;
+import com.ddubok.api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +15,23 @@ import org.springframework.stereotype.Service;
 public class CardServiceImpl implements CardService {
 
     private final CardRepository cardRepository;
+    private final MemberRepository memberRepository;
+    private final SeasonRepository seasonRepository;
 
     @Override
-    public void createCard(CreateCardReqDto req) {
-
+    public Long createCard(CreateCardReqDto dto) {
+        Card card = cardRepository.save(Card.builder()
+            .content(dto.getContent())
+            .writerName(dto.getWriterName())
+            .season(seasonRepository.findById(dto.getSeasonId()).orElseThrow(
+                () -> new SeasonNotFoundException("season not found: " + dto.getSeasonId())))
+            .path(dto.getPath())
+            .build());
+        return card.getId();
     }
 
     @Override
-    public void deleteCard(DeleteCardReq req) {
+    public void deleteCard(DeleteCardReq dto) {
 
     }
 }
