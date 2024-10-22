7f598ed40e38e72e19a4aa56e3f9c8a92aeee934
김경민 <yogurt2819@gmail.com>
2024-10-22 16:55:50 +0900

    [feat] CardNotFoundException 추가

--- backend/ddubok/src/main/java/com/ddubok/api/card/exception/CardNotFoundException.java
+++ backend/ddubok/src/main/java/com/ddubok/api/card/exception/CardNotFoundException.java
@@ -1,27 +1,35 @@
 package com.ddubok.api.card.exception;
 
+import com.ddubok.common.exception.CommonException;
+import com.ddubok.common.template.response.ResponseCode;
+
 /**
  * 해당 id의 카드가 존재하지 않을때 발생되는 에러
  */
-public class CardNotFoundException extends RuntimeException {
+public class CardNotFoundException extends CommonException {
 
-  public CardNotFoundException() {
-  }
+    @Override
+    public ResponseCode getResponseCode() {
+        return ResponseCode.CARD_NOT_FOUND;
+    }
 
-  public CardNotFoundException(String message) {
-    super(message);
-  }
+    public CardNotFoundException() {
+    }
 
-  public CardNotFoundException(String message, Throwable cause) {
-    super(message, cause);
-  }
+    public CardNotFoundException(String message) {
+        super(message);
+    }
 
-  public CardNotFoundException(Throwable cause) {
-    super(cause);
-  }
+    public CardNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-  public CardNotFoundException(String message, Throwable cause, boolean enableSuppression,
-      boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
-  }
+    public CardNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public CardNotFoundException(String message, Throwable cause, boolean enableSuppression,
+        boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
