e58e1fe4c2f854e2a28efbbece009ed8846c5fb2
qhrud7007 <qhrud7007@naver.com>
2024-10-22 14:22:13 +0900

    [feat] commonException 및 handler 구현

---
+++ backend/ddubok/src/main/java/com/ddubok/common/exception/handler/CommonExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ddubok.common.exception.handler;
+
+import com.ddubok.api.admin.exception.SeasonNotFoundException;
+import com.ddubok.api.member.exception.MemberNotFoundException;
+import com.ddubok.common.exception.CommonException;
+import com.ddubok.common.s3.exception.S3Exception;
+import com.ddubok.common.template.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler({
+        CommonException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public BaseResponse<?> handleCommonException(CommonException e) {
+        log.info(e.getMessage());
+        return BaseResponse.ofFail(e.getResponseCode());
+    }
+}
