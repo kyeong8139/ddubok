125ec537ed1d9c631dc09745e1ab7a4adb9ac4b5
김경민 <yogurt2819@gmail.com>
2024-10-22 11:15:42 +0900

    [feat] SeasonRepository 추가

---
+++ backend/ddubok/src/main/java/com/ddubok/api/admin/repository/SeasonRepository.java
@@ -0,0 +1,8 @@
+package com.ddubok.api.admin.repository;
+
+import com.ddubok.api.admin.entity.Season;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeasonRepository extends JpaRepository<Season, Long> {
+
+}
