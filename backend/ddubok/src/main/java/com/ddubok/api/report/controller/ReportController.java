30ea7e4be532c833c0308247dc50a6d1c1bd931f
박성혁 <goldenkiwi971202@gmail.com>
2024-10-23 09:57:41 +0900

    [feat] 신고기능 추가

--- backend/ddubok/src/main/java/com/ddubok/api/report/controller/ReportController.java
+++ backend/ddubok/src/main/java/com/ddubok/api/report/controller/ReportController.java
@@ -1,8 +1,13 @@
 package com.ddubok.api.report.controller;
 
+import com.ddubok.api.report.dto.request.ReportMemberReq;
 import com.ddubok.api.report.service.ReportService;
+import com.ddubok.common.template.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     final private ReportService reportService;
+
+    /**
+     * 사용자가 카드의 내용을 신고하는 경우 사용하는 API
+     * @param reportMemberReq 신고할 멤버의 객체
+     * @return 신고의 번호
+     */
+    @PostMapping("")
+    public BaseResponse<?> reportMember(
+        @RequestBody ReportMemberReq reportMemberReq){
+        System.out.println(1234);
+        System.out.println(reportMemberReq.getReportMemberId());
+        System.out.println(reportMemberReq.getCardId());
+        System.out.println(reportMemberReq.getContent());
+        Long reportId = reportService.reportMember(reportMemberReq);
+        return BaseResponse.ofSuccess(reportId);
+    }
 }
--- backend/ddubok/src/main/java/com/ddubok/api/report/service/ReportService.java
+++ backend/ddubok/src/main/java/com/ddubok/api/report/service/ReportService.java
@@ -1,8 +1,16 @@
 package com.ddubok.api.report.service;
 
+import com.ddubok.api.report.dto.request.ReportMemberReq;
+
 /**
  * 신고 관련 처리를 위한 서비스
  */
 public interface ReportService {
 
+    /**
+     * 카드의 내용을 신고
+     * @param reportMemberReq 신고할 내용을 담은 객체
+     * @return 생성된 신고의 고유 id
+     */
+    Long reportMember(ReportMemberReq reportMemberReq);
 }
