31310a4597870d57a59b3dcb887b4bb66ffe8fba
Eunicekk <cho_lyn@naver.com>
2024-11-01 10:32:55 +0900

    [fix] config 파일 수정

--- frontend/ddubok/next.config.js
+++ frontend/ddubok/next.config.js
@@ -3,10 +3,10 @@
 const nextConfig = {
 	output: "standalone",
 };
+
 const withPWA = require("next-pwa")({
 	dest: "public",
 	swDest: "public/sw.js",
-	...nextConfig,
 });
 
 module.exports = withPWA(nextConfig);
