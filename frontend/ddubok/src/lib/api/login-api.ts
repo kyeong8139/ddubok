24c6aedb6f3b6009a07c636c7e5e9c4cc456b728
Eunicekk <cho_lyn@naver.com>
2024-11-03 10:45:50 +0900

    [fix] reissue 로직 수정

--- frontend/ddubok/src/lib/api/login-api.ts
+++ frontend/ddubok/src/lib/api/login-api.ts
@@ -1,12 +1,17 @@
 "use client";
 
+import axios from "axios";
+
 import axiosInstance from "@lib/api/axiosInstance";
 
-const baseURL = process.env.NEXT_PUBLIC_LOGIN_URL;
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const loginURL = process.env.NEXT_PUBLIC_LOGIN_URL;
 
 // 토큰 재발급
 export const reissue = async () => {
-	return axiosInstance.post(`auth/reissue`);
+	return axios.post(`${baseURL}/auth/reissue`, {
+		headers: { "Content-Type": "applicaion/json" },
+	});
 };
 
 // 리프레시 토큰 확인
@@ -16,19 +21,19 @@ export const checkRefreshToken = async () => {
 
 // 로그인
 export const kakaoLogin = () => {
-	window.location.href = `${baseURL}/kakao`;
+	window.location.href = `${loginURL}/kakao`;
 };
 
 export const naverLogin = () => {
-	window.location.href = `${baseURL}/naver`;
+	window.location.href = `${loginURL}/naver`;
 };
 
 export const googleLogin = () => {
-	window.location.href = `${baseURL}/google`;
+	window.location.href = `${loginURL}/google`;
 };
 
 export const facebookLogin = () => {
-	window.location.href = `${baseURL}/facebook`;
+	window.location.href = `${loginURL}/facebook`;
 };
 
 // 로그아웃
