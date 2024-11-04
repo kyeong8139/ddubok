126a0460ce828ab83556a0a89037aa0c28dc254f
Eunicekk <cho_lyn@naver.com>
2024-11-04 14:50:29 +0900

    [refactor] 로그인 페이지 리팩토링

--- frontend/ddubok/src/styles/loginButton.ts
+++ frontend/ddubok/src/styles/loginButton.ts
@@ -2,12 +2,12 @@ export const colorClass = {
 	kakao: "bg-[#FFEB3B]",
 	naver: "bg-[#1AC904]",
 	google: "bg-[#F4F4F4]",
-	facebook: "bg-[#1878F1]",
+	x: "bg-[#000000]",
 };
 
 export const shadowClass = {
 	kakao: "shadow-kakao",
 	naver: "shadow-naver",
 	google: "shadow-google",
-	facebook: "shadow-facebook",
+	x: "shadow-x",
 };
