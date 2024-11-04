90e51b36c2f7adf4986f5862b133ed5feeaabeda
Eunicekk <cho_lyn@naver.com>
2024-11-04 18:19:35 +0900

    [feat] 신고 페이지 제작 및 API 연결

--- frontend/ddubok/src/components/card/detailCard.tsx
+++ frontend/ddubok/src/components/card/detailCard.tsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Card from "@components/card/card";
 import { ModalContext } from "@context/modal-context";
@@ -6,14 +7,20 @@ import { IDetailCardProps } from "@interface/components/card";
 import { Siren } from "@phosphor-icons/react";
 import Button from "@components/button/button";
 
-const DetailCard = ({ writer, image, content, effect }: IDetailCardProps) => {
+const DetailCard = ({ cardId, writer, image, content, effect }: IDetailCardProps) => {
+	const router = useRouter();
 	const { closeModal } = useContext(ModalContext);
+	const [showOption, setShowOption] = useState(false);
 
 	const download = () => {
 		const link = document.createElement("a");
 		link.href = image;
 		link.download = "행운카드.png";
 		link.click();
+	};
+
+	const handleReport = () => {
+		router.push(`report?cardId=${cardId}`);
 	};
 
 	return (
@@ -25,17 +32,26 @@ const DetailCard = ({ writer, image, content, effect }: IDetailCardProps) => {
 			></div>
 			<div
 				id="content"
-				className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg p-8 z-10 overflow-hidden text-center"
+				className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg z-10 overflow-hidden text-center"
 			>
 				<div className="font-nexonBold text-white flex items-center justify-between mb-4">
 					<p>From. {writer}</p>
-					<span className="bg-white rounded-full p-1 shadow-[0px_3px_0px_0px_#9E9E9E]">
+					<span
+						className="bg-white rounded-full p-1 shadow-[0px_3px_0px_0px_#9E9E9E]"
+						onClick={() => setShowOption(!showOption)}
+					>
 						<Siren
 							size={14}
 							color="red"
 							weight="fill"
 						/>
 					</span>
+					{showOption && (
+						<div className="z-10 absolute right-8 top-18 bg-white p-3 rounded-lg text-black text-right font-nexonRegular text-xs">
+							<p className="mb-2">카드 숨기기</p>
+							<p onClick={handleReport}>카드 신고하기</p>
+						</div>
+					)}
 				</div>
 				<Card
 					width={270}
---
+++ frontend/ddubok/src/lib/api/report-api.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { IReportProps } from "@interface/components/card";
+import axiosInstance from "@lib/api/axiosInstance";
+
+// 신고하기
+export const insertReport = (reportData: IReportProps) => {
+	return axiosInstance.post(`/reports`, { reportData });
+};
