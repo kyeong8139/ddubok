ad76233154444fbc58b1ae259c76a5cbdbbaf590
최다환 <sinphoto@naver.com>
2024-11-06 13:34:28 +0900

    [fix] securityConfig 허용 경로 수정

--- backend/ddubok/src/main/java/com/ddubok/common/config/SecurityConfig.java
+++ backend/ddubok/src/main/java/com/ddubok/common/config/SecurityConfig.java
@@ -180,6 +180,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/v1/cards").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v2/cards").permitAll()
                 .requestMatchers("/api/v1/cards/receive/**").permitAll()
+                .requestMatchers("/api/v1/cards/*/preview").permitAll()
                 .requestMatchers("/api/v1/cards/**").hasRole("USER")
                 .requestMatchers("/api/v1/reports").hasRole("USER")
                 .anyRequest().authenticated()
