2fc2c207a0ba694c53a6496b2481cbb51363fbc1
Eunicekk <cho_lyn@naver.com>
2024-11-14 13:42:02 +0900

    [fix] 디버깅

--- frontend/ddubok/src/app/admin/user/page.tsx
+++ frontend/ddubok/src/app/admin/user/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { useContext, useEffect, useState } from "react";
-
 import Modal from "@components/common/modal";
 import Button from "@components/button/button";
-import Loading from "@components/common/loading";
 import { ModalContext } from "@context/modal-context";
 import { IUserProps } from "@interface/components/user";
 import { selectMember, selectMemberList, updateMemberRole, updateMemberState } from "@lib/api/admin-api";
 import useAuthToken from "@lib/utils/tokenUtils";
-
 import { MagnifyingGlass } from "@phosphor-icons/react";
 import toast from "react-hot-toast";
 
@@ -21,15 +18,11 @@ const User = () => {
 	const [selectedUser, setSelectedUser] = useState<IUserProps | null>(null);
 	const [searchName, setSearchName] = useState("");
 	const [page, setPage] = useState(0);
-	const [isLoading, setIsLoading] = useState(false);
 	const [hasMore, setHasMore] = useState<boolean>(true);
 
-	const isPageReady = isLoading || !isTokenReady;
-
 	const getMemberList = async () => {
-		if (isLoading || !hasMore) return;
+		if (!hasMore) return;
 
-		setIsLoading(true);
 		try {
 			const state = selected === 1 ? "활성" : selected === 2 ? "비활성" : selected === 3 ? "차단" : null;
 			const response = await selectMemberList(state, page, 5, searchName);
@@ -42,18 +35,16 @@ const User = () => {
 			}
 		} catch (error) {
 			console.error(error);
-		} finally {
-			setIsLoading(false);
 		}
 	};
 
 	useEffect(() => {
-		if (isTokenReady && !isLoading && hasMore) getMemberList();
+		if (isTokenReady && hasMore) getMemberList();
 	}, [isTokenReady, selected, page, searchName]);
 
 	useEffect(() => {
 		const handleScroll = () => {
-			if (isLoading || !hasMore) return;
+			if (!hasMore) return;
 
 			const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 			if (scrollTop + clientHeight >= scrollHeight - 5) {
@@ -63,7 +54,7 @@ const User = () => {
 
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
-	}, [isLoading, hasMore]);
+	}, [hasMore]);
 
 	const handleClick = (index: number) => {
 		setSelected(index);
@@ -118,90 +109,84 @@ const User = () => {
 
 	return (
 		<div id="admin-user">
-			{isPageReady ? (
-				<div className="flex w-full h-screen items-center justify-center">
-					<Loading />
+			<div className="py-6">
+				<div className="text-white flex flex-col items-center">
+					<h1 className="font-nexonBold text-xl mb-2">사용자 관리</h1>
+					<p className="font-nexonRegular text-sm">가입된 사용자의 정보를 확인할 수 있어요</p>
 				</div>
-			) : (
-				<div className="py-6">
-					<div className="text-white flex flex-col items-center">
-						<h1 className="font-nexonBold text-xl mb-2">사용자 관리</h1>
-						<p className="font-nexonRegular text-sm">가입된 사용자의 정보를 확인할 수 있어요</p>
-					</div>
-					<div className="flex justify-center pt-4">
-						<ul className="bg-white font-nexonRegular inline-flex justify-center gap-1 text-xs rounded-lg p-1">
-							{["전체", "활성화", "비활성화", "차단"].map((item, index) => (
-								<li
-									key={index}
-									onClick={() => handleClick(index)}
-									className={`p-[6px] rounded-md cursor-pointer ${
-										selected === index ? "bg-ddubokPurple text-white font-nexonBold" : "bg-gray-200"
-									}`}
-								>
-									{item}
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className="bg-white flex items-center gap-2 w-[calc(100%-64px)] mx-auto my-4 p-3 rounded-lg font-nexonRegular">
-						<label htmlFor="search">
-							<MagnifyingGlass
-								size={20}
-								color="#AAAAAA"
-							/>
-						</label>
-						<input
-							id="search"
-							type="text"
-							placeholder="사용자 닉네임을 입력하세요"
-							className="text-sm w-full outline-none"
-							value={searchName}
-							onChange={(e) => setSearchName(e.target.value)}
+				<div className="flex justify-center pt-4">
+					<ul className="bg-white font-nexonRegular inline-flex justify-center gap-1 text-xs rounded-lg p-1">
+						{["전체", "활성화", "비활성화", "차단"].map((item, index) => (
+							<li
+								key={index}
+								onClick={() => handleClick(index)}
+								className={`p-[6px] rounded-md cursor-pointer ${
+									selected === index ? "bg-ddubokPurple text-white font-nexonBold" : "bg-gray-200"
+								}`}
+							>
+								{item}
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="bg-white flex items-center gap-2 w-[calc(100%-64px)] mx-auto my-4 p-3 rounded-lg font-nexonRegular">
+					<label htmlFor="search">
+						<MagnifyingGlass
+							size={20}
+							color="#AAAAAA"
 						/>
-					</div>
-					<div>
-						<table className="text-white  font-nexonRegular w-[calc(100%-64px)] mx-auto">
-							<thead>
-								<tr className="text-xs border-b-2 border-solid border-white">
-									<th className="px-2 py-[10px]">번호</th>
-									<th className="px-2 py-[10px]">닉네임</th>
-									<th className="px-2 py-[10px]">상세</th>
-								</tr>
-							</thead>
-							<tbody>
-								{userList.length > 0 ? (
-									userList.map((user) => (
-										<tr
-											key={user.memberId}
-											className="text-center text-xs border-b-[1px] border-solid border-white"
-										>
-											<td className="px-2 py-[10px]">{user.memberId}</td>
-											<td className="px-2 py-[10px]">{user.nickname}</td>
-											<td className="px-2 py-[10px]">
-												<button
-													className="underline"
-													onClick={() => handleDetailClick(user.memberId)}
-												>
-													상세보기
-												</button>
-											</td>
-										</tr>
-									))
-								) : (
-									<tr>
-										<td
-											colSpan={3}
-											className="text-center py-[10px]"
-										>
-											데이터가 없습니다.
+					</label>
+					<input
+						id="search"
+						type="text"
+						placeholder="사용자 닉네임을 입력하세요"
+						className="text-sm w-full outline-none"
+						value={searchName}
+						onChange={(e) => setSearchName(e.target.value)}
+					/>
+				</div>
+				<div>
+					<table className="text-white font-nexonRegular w-[calc(100%-64px)] mx-auto">
+						<thead>
+							<tr className="text-xs border-b-2 border-solid border-white">
+								<th className="px-2 py-[10px]">번호</th>
+								<th className="px-2 py-[10px]">닉네임</th>
+								<th className="px-2 py-[10px]">상세</th>
+							</tr>
+						</thead>
+						<tbody>
+							{userList.length > 0 ? (
+								userList.map((user) => (
+									<tr
+										key={user.memberId}
+										className="text-center text-xs border-b-[1px] border-solid border-white"
+									>
+										<td className="px-2 py-[10px]">{user.memberId}</td>
+										<td className="px-2 py-[10px]">{user.nickname}</td>
+										<td className="px-2 py-[10px]">
+											<button
+												className="underline"
+												onClick={() => handleDetailClick(user.memberId)}
+											>
+												상세보기
+											</button>
 										</td>
 									</tr>
-								)}
-							</tbody>
-						</table>
-					</div>
+								))
+							) : (
+								<tr>
+									<td
+										colSpan={3}
+										className="text-center py-[10px]"
+									>
+										데이터가 없습니다.
+									</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
 				</div>
-			)}
+			</div>
 			<div
 				className={`transition-opacity duration-300 ${
 					isModalOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
